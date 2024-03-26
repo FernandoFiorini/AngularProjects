dd6864e19dcce6f9226186de5d8d52cd55eab539
Fernando Fiorini <fernandofiorini157@gmail.com>
2024-03-26 18:15:15 -0300

    Ajustes musicas.

--- src/app/music-card/music-card.component.ts
+++ src/app/music-card/music-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 
 
@@ -11,68 +12,81 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class MusicCardComponent implements OnInit {
 
   @ViewChild('seekbar') progressBar: any;
+  @ViewChild('player') musicPlayer: any;
 
+  repeatMusic = false;
+  randomMusic = false;
   musicPlaying = false;
-  currentMusic = '';
+
+  currentMusic: any;
   currentPosition = 0;
+
   musicList = [
-    '../../assets/MusicCard/music/music1.mp3',
-    '../../assets/MusicCard/music/music2.mp3',
-    '../../assets/MusicCard/music/music3.mp3',
-    '../../assets/MusicCard/music/music4.mp3']
+    {
+      name: 'Lofi Study',
+      artist: 'FASSounds',
+      music: '../../assets/MusicCard/music/lofiStudy.mp3',
+      image: '../../assets/MusicCard/FASSSounds.jpg'
+    }, {
+      name: 'Spirit Blossom',
+      artist: 'RomanBelov',
+      music: '../../assets/MusicCard/music/spiritBlossom.mp3',
+      image: '../../assets/MusicCard/music.jpg'
+    }, {
+      name: 'Once In Paris',
+      artist: 'Pumpupthemind',
+      music: '../../assets/MusicCard/music/onceInParis.mp3',
+      image: './../assets/MusicCard/onceInParis.jpg'
+    }, {
+      name: 'Good Night',
+      artist: 'FASSounds',
+      music: '../../assets/MusicCard/music/goodNight.mp3',
+      image: './../assets/MusicCard/FASSSounds.jpg'
+    }]
 
 
   ngOnInit(): void {
     this.currentMusic = this.musicList[this.currentPosition];
 
-    // this.progressBar.nativeElement.addEventListener("timeupdate", (time: any) => {
-    //   this.atualizarProgresso();
-    // });
-
-
-    this.timeUpdateRegister();
-
+    this.initialMusicTime();
   }
 
-  timeUpdateRegister() {
+  initialMusicTime() {
+    this.atualizarProgresso();
+  }
 
-    var player: any = <HTMLVideoElement>document.getElementById('player');
-    player.addEventListener("timeupdate", function () {
+  changeProgress(event: any) {
 
-      var progressBar: any = <HTMLVideoElement>document.getElementById('seekbar');
-      var tempo: any = <HTMLVideoElement>document.getElementById('tempo');
+    let me = this;
 
-      var currentTime = player.currentTime;
-      var duration = player.duration;
-      progressBar.value = player.currentTime / player.duration;
+    var player = me.musicPlayer.nativeElement;
 
+    var progress = me.progressBar.nativeElement;
 
+    var pos = event.pageX - progress.offsetLeft;
+    var percent = pos / progress.clientWidth;
 
-    });
+    player.currentTime = percent * player.duration;
+    progress.value = percent / 100;
   }
 
   formatTime(tempo: any) {
     const seconds = tempo.toFixed(0);
-
     const h = Math.floor(seconds / 3600)
     const m = Math.floor((seconds % 3600) / 60)
     const s = seconds % 60
     return [h, m > 9 ? m : h ? '0' + m : m || '0', s > 9 ? s : '0' + s]
       .filter(a => a)
       .join(':')
+
   }
 
 
   playMusic() {
     const me = this;
-    const player = <HTMLVideoElement>document.getElementById('player');
-    const tempo = <HTMLVideoElement>document.getElementById('tempo');
+    const player = me.musicPlayer.nativeElement;
 
     player.volume = 0.2;
-
-
-    tempo.innerHTML = this.formatTime(player.duration)
-
 
     if (me.musicPlaying) {
       me.musicPlaying = false;
@@ -85,22 +99,56 @@ export class MusicCardComponent implements OnInit {
   }
 
   previousMusic() {
-   
-    this.currentPosition--;
-    this.currentMusic = this.musicList[this.currentPosition]
-    this.timeUpdateRegister();
+
+    if (this.currentPosition > 0 && this.currentPosition <= this.musicList.length) {
+      this.currentPosition--;
+      this.currentMusic = this.musicList[this.currentPosition]
+      this.musicPlaying = false;
+    }
+
   }
 
   nextMusic() {
 
-    this.currentPosition--;
-    this.currentMusic = this.musicList[this.currentPosition]
-    this.timeUpdateRegister();
+    if (this.currentPosition <= this.musicList.length) {
+      this.currentPosition++;
+      this.currentMusic = this.musicList[this.currentPosition];
+      this.musicPlaying = false;
+    }
+
   }
 
   atualizarProgresso() {
-    const progressBar = <HTMLVideoElement>document.getElementById('seekbar');
+    const me: any = this;
+
+    var player = me.musicPlayer.nativeElement;
+    var progress = me.progressBar.nativeElement;
+    var tempo: any = <HTMLVideoElement>document.getElementById('tempo');
+
+    var currentTime = player.currentTime;
+    var duration = player.duration;
 
 
+    progress.value = currentTime / duration;
+
+    let tempoAtual = me.formatTime(currentTime);
+    let tempoTotal = me.formatTime(duration);
+
+    if (currentTime == "0:00") {
+      tempo.innerHTML = "0:00" + "/" + tempoTotal;
+    } else {
+
+      tempo.innerHTML = tempoAtual + "/" + tempoTotal;
+    }
+
+
+  }
+
+  repeat() {
+    this.repeatMusic = !this.repeatMusic;
+  }
+
+  random() {
+    this.randomMusic = !this.randomMusic;
   }
 }
